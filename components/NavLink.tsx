0e48ec474f4d43332d1b6d4d518e6cdfa2b16339
Ashimolowo <ashimolowoayobami@gmail.com>
2025-01-30 04:13:42 -0800

    Navbar created successfully

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -6,14 +6,14 @@ import { usePathname } from "next/navigation";
 interface NavLinkProps {
   href: string;
   label: string;
-  
+  icon?: string;  
 }
-const NavLink: React.FC<NavLinkProps> = ({href, label, }) => {
+const NavLink: React.FC<NavLinkProps> = ({href, label,icon }) => {
     const pathname = usePathname()
   return (
     <div>
         <Link href={href} className={` lg:w-36 text-[#663BC4] ${pathname === href ? " text-[#481DA6] " : ""} hover:text-[#a189d6]`}> 
-         {label}
+         {icon}{label}
         </Link>
     </div>
   )
